0d083ec2515344f2e4e10d9d38f32448ace7459d
bmancini55 <bmancini@gmail.com>
2013-12-24 11:07:42 -0500

    Added configuration files

--- app.js
+++ app.js
@@ -1,23 +1,69 @@
 // module requirements
-var express = require('express'),
-    hbs = require('express-hbs'),
+var express = require('express')
+  , CONFIG = require('./config.js').development
+  , hbs = require('express-hbs')
 
 // local variables
-    app = express();
+  , app = express();
 
 // configure view engine
 app.set('view engine', 'hbs');
+
+// configure hbs
 app.engine('hbs', hbs.express3({
-    layout: 'layout.hbs'
+  defaultLayout: __dirname + '/views/layout.hbs'
 }));
+
+// configure view directory
 app.set('views', __dirname + '/views');
 
-// configure static files
-app.use(__dirname + '/static');
+// configure static file directory
+app.use('/public', express.static(__dirname + '/public'));
 
 
 // CONFIGURE REQUESTS
 app.get('/', function(req, res) {
-    res.render('index.hbs');
+  res.render('index.hbs');
 });
-    
+
+
+
+// BITCOIN SHIZZY
+getBitcoinClient = function(cb) {
+  var bitcoin = require('bitcoin')
+    , clientConfig = CONFIG.clients.bitcoind
+    , client;    
+
+  client = new bitcoin.Client({
+    host: clientConfig.host,
+    port: clientConfig.port,
+    user: clientConfig.user,
+    pass: clientConfig.pass
+  });
+
+  process.nextTick(function() {
+      cb(null,client)
+  });
+}
+
+app.get('/bitcoin/accounts', function(req, res) {
+  getBitcoinClient(function(err, client) {
+    client.listAccounts(function(err, result) {
+      if(err) res.send(err);
+      else res.send(result);    
+    });
+  });
+
+});
+
+app.get('/bitcoin/transactions', function(req, res) {
+  getBitcoinClient(function(err, client) {
+    client.listTransactions(function(err, result) {
+      if(err) res.send(err);
+      else res.send(result);    
+    });
+  });
+});
+
+console.log('listening on ' + CONFIG.server.port);
+app.listen(CONFIG.server.port);
