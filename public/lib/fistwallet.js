b17efc9ba5e96c569f9af227a7e42ffc2acd5764
bmancini55 <bmancini@gmail.com>
2013-12-26 15:25:23 -0500

    Added additional scripts

---
+++ public/lib/fistwallet.js
@@ -0,0 +1,24 @@
+// globals window
+
+window.FistWallet = (function() {
+  'use strict';
+  
+  return {
+
+    /**
+     * Converts an ECKey object to an BIP38 encrypted
+     */
+    encryptBIP38ToBase58: function(eckey, passphrase, success, error) {
+      
+    },
+
+    /**
+     * Decrypts an ECKey object from BIP38 base58 stream
+     */
+    decryptBIP38ToECKey: function(base58bip38, passphrase, success, error) {
+      
+    }
+
+  }
+
+}());
